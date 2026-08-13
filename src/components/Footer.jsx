28b6d335f6b2d1e10a07b45dde03a6e66de2e05e
Reese_Ruffin <64818774+TruSigns@users.noreply.github.com>
2026-08-13 00:10:09 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} [Your Name]. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} Tru$igns All rights reserved.</p>
     </footer>
   );
 }
